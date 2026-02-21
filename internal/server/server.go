6ae10aa83737349752c3639d98502c2ba8a721e8
Karan <karanlokchandani@protonmail.com>
2026-02-22 02:28:47 +0530

    refactor(server): extract udp listener setup

--- internal/server/server.go
+++ internal/server/server.go
@@ -83,27 +83,9 @@ func (s *Server) Start(ctx context.Context) error {
 	cacheResolver, _ := s.resolver.(types.CacheResolver)
 
 	for _, addr := range s.cfg.ListenAddrs {
-		for i := 0; i < udpSockets; i++ {
-			conn, err := listenUDPPacket(addr, udpSockets > 1)
-			if err != nil {
-				shutdown()
-				return err
-			}
-			w := &udpWriter{conn: conn, ch: make(chan udpWrite, s.cfg.Workers)}
-			writersWg.Add(1)
-			go func(writer *udpWriter) {
-				defer writersWg.Done()
-				s.udpWriteLoop(ctx, writer)
-			}(w)
-			if i == 0 {
-				s.logger.Info("dns server listening", "listen", addr, "udp_sockets", udpSockets)
-			}
-
-			readersWg.Add(1)
-			go func(writer *udpWriter) {
-				defer readersWg.Done()
-				s.udpReadLoop(ctx, writer, cacheResolver)
-			}(w)
+		if err := s.startUDPListeners(ctx, addr, udpSockets, cacheResolver, &readersWg, &writersWg); err != nil {
+			shutdown()
+			return err
 		}
 
 		if err := s.startTCPListener(ctx, addr, &readersWg); err != nil {
